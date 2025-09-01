8ce35a318ebda96b430f0b5c2d0881feda8fc709
mamebassine <mamebassine06@gmail.com>
2025-09-01 16:38:44 +0000

    Apres mis en jour de 10 a 11

--- src/views/auth_user/Dashboard.vue
+++ src/views/auth_user/Dashboard.vue
@@ -11,12 +11,5 @@
 </script>
 
 <style scoped>
-/* .dashboard {
-  max-width: 600px;
-  margin: auto;
-  padding: 1em;
-  background-color: #fff;
-  border-radius: 8px;
-  box-shadow: 0 2px 8px rgba(0,0,0,0.1);
-} */
+
 </style>
