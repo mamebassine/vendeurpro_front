fb2222b63a19d4932b73842cff0dac073c7234bb
mamebassine <mamebassine06@gmail.com>
2025-09-24 01:41:55 +0000

    Fatiguer email

--- src/services/api.js
+++ src/services/api.js
@@ -14,7 +14,9 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://api.vendeur-pro.com/api', 
+  // baseURL: 'http://api.vendeur-pro.com/api', 
+    baseURL: 'https://api.vendeur-pro.com/api', // HTTPS sécurisé
+
   withCredentials: false,
   headers: {
     'Content-Type': 'application/json',
