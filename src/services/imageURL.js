39d2c74ccc24dbbe35840d1a1b4c35ea35a41386
mamebassine <mamebassine06@gmail.com>
2025-09-24 16:45:50 +0000

    Fatiguer email

--- src/services/imageURL.js
+++ src/services/imageURL.js
@@ -1,3 +1,3 @@
 // services/imageURL.js
-const urlImage = "http://api.vendeur-pro.com/vendeurPro_back/storage/app/public/";
+const urlImage = "https://api.vendeur-pro.com/vendeurPro_back/storage/app/public/";
 export default urlImage;
